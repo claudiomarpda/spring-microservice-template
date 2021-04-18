3b956a7aa4b9581b9b6a1a22088e3a8f24532182
Claudiomar Araujo <claudiomar.development@gmail.com>
2021-04-18 17:16:54 -0300

    Request Post from Timeline with FeignClient

---
+++ timeline-service/src/main/java/com/micro/timelineservice/TimelineServiceApplication.java
@@ -0,0 +1,58 @@
+package com.micro.timelineservice;
+
+import feign.FeignException;
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+
+@EnableFeignClients
+@SpringBootApplication
+public class TimelineServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimelineServiceApplication.class, args);
+    }
+}
+
+@RestController
+@RequestMapping("/api")
+class TimelineController {
+    PostClient postClient;
+
+    public TimelineController(PostClient pc) {
+        this.postClient = pc;
+    }
+
+    @PostMapping("/posts/{postId}/process")
+    Post processPost(@PathVariable Long postId) {
+        return postClient.findById(postId);
+    }
+}
+
+@FeignClient(value = "posts", url = "${post-service.url}")
+interface PostClient {
+
+    @GetMapping("/{postId}")
+    Post findById(@PathVariable Long postId);
+}
+
+@Data
+class Post {
+    Long id;
+    String description;
+}
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(FeignException.class)
+    String handleFeignClientException(FeignException e, HttpServletResponse response) {
+        response.setStatus(e.status());
+        return e.contentUTF8();
+    }
+}
