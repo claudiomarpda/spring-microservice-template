0e2e3855853a1b97134202462c54fe0739f8bab0
Claudiomar Araujo <claudiomar.development@gmail.com>
2021-04-21 13:41:25 -0300

    Centralized config

---
+++ central-config/src/main/java/com/micro/centralconfig/CentralConfigApplication.java
@@ -0,0 +1,15 @@
+package com.micro.centralconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class CentralConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralConfigApplication.class, args);
+	}
+
+}
---
+++ central-config/src/test/java/com/micro/centralconfig/CentralConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.centralconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
