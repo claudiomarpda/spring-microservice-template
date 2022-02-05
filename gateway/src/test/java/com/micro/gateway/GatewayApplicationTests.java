09c98cac6e3228383673920a42ebb33f2de1a9bf
Claudiomar Araujo <claudiomar.development@gmail.com>
2022-02-05 10:33:44 -0300

    feat: central config variables

--- gateway/src/test/java/com/micro/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/micro/gateway/GatewayApplicationTests.java
@@ -2,9 +2,7 @@ package com.micro.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootTest
 class GatewayApplicationTests {
 
